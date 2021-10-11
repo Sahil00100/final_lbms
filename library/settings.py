891b01bce0011e3e9bc1aeacc76f1997016dba0b
Sahil <88336191+Sahil00100@users.noreply.github.com>
2021-10-11 20:19:58 +0530

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d128-90yyo(makj10^3f-m$o)kjvncj2w@u=6z5o+%k+#7rq^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['library-lbm.herokuapp.com','localhost','127.0.0.1','my-lbms.herokuapp.com']
 
@@ -133,11 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
  'static'
 ]
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
