62f2a2e6a0f89705936ad29143b43770031249a5
Sahil <88336191+Sahil00100@users.noreply.github.com>
2021-10-11 20:02:35 +0530

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d128-90yyo(makj10^3f-m$o)kjvncj2w@u=6z5o+%k+#7rq^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['library-lbm.herokuapp.com','localhost','127.0.0.1','my-lbms.herokuapp.com']
 
