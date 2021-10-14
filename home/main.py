ee52351f68a405ad7c61355e6c3f3a7dc65dff65
Sahil <88336191+Sahil00100@users.noreply.github.com>
2021-10-14 11:04:08 +0530

    Update main.py

--- home/main.py
+++ home/main.py
@@ -1,6 +1,1 @@
-import razorpay
-client = razorpay.Client(auth=("rzp_test_HILSu4mI2BIBak", "I8hWFGpL2SbFPHXNwkLisHzU"))
 
-data = { "amount": 500, "currency": "INR", "receipt": "order_rcptid_11" }
-
-payment = client.order.create(data=data)
